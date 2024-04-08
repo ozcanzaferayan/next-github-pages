eca07bec05816a0fb160357cf6c916844a9213aa
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-04-08 17:05:46 +0300

    Add static file content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
-const page = () => {
+import Image from "next/image";
+
+async function getPokemons() {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+  const pokemons = await res.json();
+
+  const promises = await Promise.allSettled(
+    pokemons.results.map((pokemon: any) => fetchPokemonData(pokemon)),
+  );
+  return promises.map((promise: any) => promise.value);
+}
+const fetchPokemonData = async (pokemon: any) => {
+  let url = pokemon.url;
+  const res = await fetch(url);
+  const data = await res.json();
+  return {
+    name: data.name,
+    image: data.sprites.front_default,
+    types: data.types.map((typeInfo: any) => typeInfo.type.name),
+  };
+};
+
+const page = async () => {
+  const pokemons = await getPokemons();
   return (
-    <div className="flex h-dvh w-full items-center justify-center">
-      <h1
-        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-6xl font-bold text-transparent
-      "
-      >
-        SPA with NextJS
-      </h1>
+    <div className="flex h-dvh w-full flex-wrap gap-4 bg-slate-50">
+      {pokemons.map((pokemon: any) => (
+        <div key={pokemon.name} className="flex flex-1 flex-col ">
+          <Image
+            className="h-24 w-24"
+            src={pokemon.image}
+            alt={pokemon.name}
+            width={50}
+            height={50}
+          />
+          <h1 className="text-2xl font-bold">{pokemon.name}</h1>
+        </div>
+      ))}
     </div>
   );
 };
