e8c7469e624bd28d021e27ffa5897523f5862d6b
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-04-08 17:12:16 +0300

    Fix basePath in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "next-github-pages",
+  basePath: "/next-github-pages",
   output: "export",
   images: {
     unoptimized: true,
